05c0f1d59bcd7cb0a7f96c171609e5580ecc8927
Ch3rrygupta <cherryguptask@gmail.com>
2024-12-04 14:07:38 -0600

    added the display grid part

--- sketch.js
+++ sketch.js
@@ -3,16 +3,6 @@
 // Due sometime in january!!!
 
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
-  background(220);
-
-  circle(mouseX, mouseY, 100);
-}
-
 let grid = [
   [0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0],
@@ -83,4 +73,41 @@ let oneSquareBlock = [
   [1]
 ];
 
+const GRID_SIZE = 8;
+let cellSize = 50;
 
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  if (windowWidth < windowHeight) {
+    createCanvas(windowWidth, windowWidth);
+  }
+  else {
+    createCanvas(windowHeight, windowHeight);
+  }
+}
+
+function draw() {
+  background(220);
+  //draw the grid
+  displayGrid();
+
+  circle(mouseX, mouseY, 100);
+}
+
+
+// display the grid
+function displayGrid() {
+  for (let y = 0; y < GRID_SIZE; y++) {
+    for (let x = 0; x < GRID_SIZE; x++) {
+      if (grid[y][x] === 1) {
+        fill("black");
+      }
+      else if (grid[y][x] === 0) {
+        fill("white");
+      }
+      square(x * cellSize, y * cellSize, cellSize);
+    }
+  }
+}
+
+// make the drag and drop block section

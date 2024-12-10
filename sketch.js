39e92712aab7c38d42911d4cfc3298f86b9ebe4b
Ch3rrygupta <cherryguptask@gmail.com>
2024-12-10 13:15:33 -0600

    Figured out the logic last night, I just forgot to commit, next I will focus on fixing the  clicking issue and work on the visuals of the game cos its kinda ugly.

--- sketch.js
+++ sketch.js
@@ -3,17 +3,6 @@
 // Due sometime in january!!!
 
 
-let grid = [
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-];
-
 let bigLBlock = [
   [1, 0, 0],
   [1, 0, 0],
@@ -73,72 +62,137 @@ let oneSquareBlock = [
   [1]
 ];
 
-const GRID_SIZE = 8;
+let grid = [];
+let gridSize = 8;
 let cellSize = 50;
+let currentBlocks = [];
+let score = 0;
+let selectedBlock = null;
+let mouseOffset = { x: 0, y: 0 };
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  if (windowWidth < windowHeight) {
-    createCanvas(windowWidth, windowWidth);
-  }
-  else {
-    createCanvas(windowHeight, windowHeight);
-  }
+  createCanvas(gridSize * cellSize + 200, gridSize * cellSize);
+  initializeGrid();
+  generateBlocks();
 }
 
 function draw() {
-  background(220);
-  //draw the grid
-  displayGrid();
-
-  circle(mouseX, mouseY, 100);
+  background(184,245, 255);
+  drawGrid();
+  drawBlocks();
+  displayScore();
 }
 
+function initializeGrid() {
+  for (let i = 0; i < gridSize; i++) {
+    grid[i] = [];
+    for (let j = 0; j < gridSize; j++) {
+      grid[i][j] = 0;
+    }
+  }
+}
 
-// display the grid
-function displayGrid() {
-  for (let y = 0; y < GRID_SIZE; y++) {
-    for (let x = 0; x < GRID_SIZE; x++) {
-      if (grid[y][x] === 1) {
-        fill("black");
+function drawGrid() {
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      fill(grid[i][j] ? 'pink' : 'white');
+      stroke(0);
+      rect(j * cellSize, i * cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+function generateBlocks() {
+  currentBlocks = [];
+  for (let i = 0; i < 3; i++) {
+    let block = [];
+    let blockSize = int(random(1, 4)); // Random block size (1 to 3 cells)
+    for (let j = 0; j < blockSize; j++) {
+      block.push([j, 0]); // Simple horizontal blocks for simplicity
+    }
+    currentBlocks.push(block);
+  }
+}
+
+function drawBlocks() {
+  let xOffset = gridSize * cellSize + 20;
+  for (let i = 0; i < currentBlocks.length; i++) {
+    let block = currentBlocks[i];
+    for (let j = 0; j < block.length; j++) {
+      let x = xOffset + block[j][0] * cellSize + (i === selectedBlock ? mouseX - mouseOffset.x : 0);
+      let y = 100 + i * 100 + block[j][1] * cellSize + (i === selectedBlock ? mouseY - mouseOffset.y : 0);
+      fill('purple');
+      stroke(0);
+      rect(x, y, cellSize, cellSize);
+    }
+  }
+}
+
+function displayScore() {
+  fill(0);
+  textSize(16);
+  text('Score: ' + score, gridSize * cellSize + 20, 50);
+}
+
+function mousePressed() {
+  let xOffset = gridSize * cellSize + 20;
+  for (let i = 0; i < currentBlocks.length; i++) {
+    let block = currentBlocks[i];
+    for (let j = 0; j < block.length; j++) {
+      let x = xOffset + block[j][0] * cellSize;
+      let y = 100 + i * 100 + block[j][1] * cellSize;
+      if (mouseX > x && mouseX < x + cellSize && mouseY > y && mouseY < y + cellSize) {
+        selectedBlock = i;
+        mouseOffset.x = mouseX;
+        mouseOffset.y = mouseY;
+        return;
       }
-      else if (grid[y][x] === 0) {
-        fill("white");
+    }
+  }
+}
+
+function mouseReleased() {
+  if (selectedBlock !== null) {
+    placeBlock(selectedBlock);
+    selectedBlock = null;
+  }
+}
+
+function placeBlock(blockIndex) {
+  let block = currentBlocks[blockIndex];
+  let gridX = floor(mouseX / cellSize);
+  let gridY = floor(mouseY / cellSize);
+
+  if (gridX >= 0 && gridX < gridSize && gridY >= 0 && gridY < gridSize) {
+    for (let i = 0; i < block.length; i++) {
+      let x = gridX + block[i][0];
+      let y = gridY + block[i][1];
+      if (x < gridSize && y < gridSize && grid[y][x] === 0) {
+        grid[y][x] = 1;
+      } else {
+        return;
       }
-      square(x * cellSize, y * cellSize, cellSize);
+    }
+    currentBlocks.splice(blockIndex, 1);
+    checkRowsAndColumns();
+    if (currentBlocks.length === 0) {
+      generateBlocks();
     }
   }
 }
 
-// make the drag and drop block section
- 
-
-// clear the rows if they all get full
-function rowCleared(){
-
-  //idk if this approach will even work
-  for (let y = 0; y < GRID_SIZE; y++) {
-    if(grid[y] === 1){
-      grid[y] === 0;
+function checkRowsAndColumns() {
+  for (let i = 0; i < gridSize; i++) {
+    if (grid[i].every(cell => cell === 1)) {
+      grid[i] = grid[i].map(() => 0);
+      score += gridSize;
     }
-  }
-
-  for (let x = 0; x < GRID_SIZE; x++) {
-    if(grid[x] === 1){
-      grid[x] === 0;
-    }
-  }
-
-  // another approach I could try
-  cellCounter = 0;
-
-  for (let y = 0; y < GRID_SIZE; y++) {
-    for (let x = 0; x < GRID_SIZE; x++) {
-      // idk
+    if (grid.every(row => row[i] === 1)) {
+      for (let j = 0; j < gridSize; j++) {
+        grid[j][i] = 0;
+      }
+      score += gridSize;
     }
   }
 }
 
-
-
-// 

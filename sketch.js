4950c470665ab3e68617a8eb1939f0f0527bfc88
Ch3rrygupta <cherryguptask@gmail.com>
2025-01-16 11:28:12 -0600

    Figured out how to use the preset shapes in the game

--- sketch.js
+++ sketch.js
@@ -1,12 +1,27 @@
-// Block Blast
-// Cherry Gupta
-// Due January 21st, Presenting January 16th
-
+// BlockBlast Game with OOP, Drag-and-Drop, Start Screen, and End Screen
 
 class Block {
   constructor(shape, color) {
     this.shape = shape;
     this.color = color;
+    this.x = 0;
+    this.y = 0;
+    this.isDragging = false;
+  }
+
+  containsPoint(px, py, cellSize) {
+    for (let row = 0; row < this.shape.length; row++) {
+      for (let col = 0; col < this.shape[row].length; col++) {
+        if (this.shape[row][col] === 1) {
+          let x = this.x + col * cellSize;
+          let y = this.y + row * cellSize;
+          if (px > x && px < x + cellSize && py > y && py < y + cellSize) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
   }
 }
 
@@ -18,260 +33,291 @@ class Game {
     this.currentBlocks = [];
     this.score = 0;
     this.highScore = 0;
-    this.selectedBlock = null;
-    this.mouseOffset = { x: 0, y: 0 };
+    this.draggedBlock = null;
+    this.offset = { x: 0, y: 0 };
     this.colors = ["yellow", "pink", "purple", "green", "blue", "orange"];
     this.state = "start"; // start, playing, end
   }
+
+  initializeGrid() {
+    for (let i = 0; i < this.gridSize; i++) {
+      this.grid[i] = [];
+      for (let j = 0; j < this.gridSize; j++) {
+        this.grid[i][j] = 0;
+      }
+    }
+  }
+
+  generateBlocks() {
+    this.currentBlocks = [];
+    for (let i = 0; i < 3; i++) {
+      let randomIndex = floor(random(blockShapes.length));
+      let shape = blockShapes[randomIndex];
+      let color = this.colors[i % this.colors.length];
+      let block = new Block(shape, color);
+      block.x = this.gridSize * this.cellSize + 40;
+      block.y = 100 + i * 120;
+      this.currentBlocks.push(block);
+    }
+  }
+
+  drawGrid() {
+    for (let i = 0; i < this.gridSize; i++) {
+      for (let j = 0; j < this.gridSize; j++) {
+        fill(this.grid[i][j] ? "black" : "white");
+        stroke(0);
+        rect(j * this.cellSize + 20, i * this.cellSize + this.cellSize, this.cellSize, this.cellSize);
+      }
+    }
+  }
+
+  drawBlocks() {
+    for (let block of this.currentBlocks) {
+      for (let row = 0; row < block.shape.length; row++) {
+        for (let col = 0; col < block.shape[row].length; col++) {
+          if (block.shape[row][col] === 1) {
+            let x = block.x + col * this.cellSize;
+            let y = block.y + row * this.cellSize;
+            fill(block.color);
+            stroke(0);
+            rect(x, y, this.cellSize, this.cellSize);
+          }
+        }
+      }
+    }
+  }
+
+  displayScore() {
+    fill(0);
+    textSize(16);
+    text("Score: " + this.score, this.gridSize * this.cellSize + 40, 50);
+    text("High Score: " + this.highScore, this.gridSize * this.cellSize + 40, 70);
+  }
+
+  canPlaceBlock(block, gridX, gridY) {
+    for (let row = 0; row < block.shape.length; row++) {
+      for (let col = 0; col < block.shape[row].length; col++) {
+        if (block.shape[row][col] === 1) {
+          let x = gridX + col;
+          let y = gridY + row;
+          if (
+            x < 0 ||
+            x >= this.gridSize ||
+            y < 0 ||
+            y >= this.gridSize ||
+            this.grid[y][x] === 1
+          ) {
+            return false;
+          }
+        }
+      }
+    }
+    return true;
+  }
+
+  placeBlock(block) {
+    let gridX = floor((block.x - 20) / this.cellSize);
+    let gridY = floor((block.y - this.cellSize) / this.cellSize);
+
+    if (this.canPlaceBlock(block, gridX, gridY)) {
+      for (let row = 0; row < block.shape.length; row++) {
+        for (let col = 0; col < block.shape[row].length; col++) {
+          if (block.shape[row][col] === 1) {
+            this.grid[gridY + row][gridX + col] = 1;
+          }
+        }
+      }
+      this.currentBlocks.splice(this.currentBlocks.indexOf(block), 1);
+      this.checkRowsAndColumns();
+      if (this.currentBlocks.length === 0) {
+        this.generateBlocks();
+      }
+    }
+  }
+
+  checkRowsAndColumns() {
+    for (let i = 0; i < this.gridSize; i++) {
+      if (this.grid[i].every((cell) => cell === 1)) {
+        this.grid[i] = this.grid[i].map(() => 0);
+        this.score += this.gridSize;
+      }
+    }
+    for (let j = 0; j < this.gridSize; j++) {
+      if (this.grid.every((row) => row[j] === 1)) {
+        for (let i = 0; i < this.gridSize; i++) {
+          this.grid[i][j] = 0;
+        }
+        this.score += this.gridSize;
+      }
+    }
+  }
+
+  canPlaceAnyBlock() {
+    for (let block of this.currentBlocks) {
+      for (let y = 0; y < this.gridSize; y++) {
+        for (let x = 0; x < this.gridSize; x++) {
+          if (this.canPlaceBlock(block, x, y)) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  drawStartScreen() {
+    background(184, 245, 255);
+    fill(0);
+    textAlign(CENTER, CENTER);
+    textSize(48);
+    text("BlockBlast", width / 2, height / 2 - 50);
+    textSize(24);
+    text("Click to Start", width / 2, height / 2);
+  }
+
+  drawEndScreen() {
+    background(184, 245, 255);
+    fill(0);
+    textAlign(CENTER, CENTER);
+    textSize(48);
+    text("Game Over", width / 2, height / 2 - 50);
+    textSize(24);
+    text("Score: " + this.score, width / 2, height / 2);
+    text("High Score: " + this.highScore, width / 2, height / 2 + 30);
+    text("Click to Play Again", width / 2, height / 2 + 60);
+  }
+
+  resetGame() {
+    this.score = 0;
+    this.initializeGrid();
+    this.generateBlocks();
+    this.state = "playing";
+  }
+
+  update() {
+    if (this.state === "playing" && !this.canPlaceAnyBlock()) {
+      this.state = "end";
+      this.highScore = max(this.highScore, this.score);
+    }
+  }
+
+  draw() {
+    if (this.state === "start") {
+      this.drawStartScreen();
+    } else if (this.state === "playing") {
+      background(184, 245, 255);
+      this.drawGrid();
+      this.drawBlocks();
+      this.displayScore();
+    } else if (this.state === "end") {
+      this.drawEndScreen();
+    }
+  }
 }
 
-let bigLBlock = [
-  [1, 0, 0],
-  [1, 0, 0],
-  [1, 1, 1]
+let game;
+let blockShapes = [
+  [
+    [1, 0, 0],
+    [1, 0, 0],
+    [1, 1, 1]
+  ],
+  [
+    [0, 0, 1],
+    [0, 0, 1],
+    [1, 1, 1]
+  ],
+  [
+    [1, 0],
+    [1, 0],
+    [1, 1]
+  ],
+  [
+    [0, 1],
+    [0, 1],
+    [1, 1]
+  ],
+  [
+    [1, 0],
+    [1, 1]
+  ],
+  [
+    [0, 1],
+    [1, 1]
+  ],
+  [
+    [1, 1, 1],
+    [1, 1, 1],
+    [1, 1, 1]
+  ],
+  [
+    [1, 1],
+    [1, 1]
+  ],
+  [
+    [1, 1, 1],
+    [0, 1, 0]
+  ],
+  [
+    [1, 1, 1]
+  ],
+  [
+    [1, 1, 1, 1]
+  ],
+  [
+    [1, 1, 1, 1, 1]
+  ],
+  [
+    [0, 1, 1],
+    [1, 1, 0]
+  ],
+  [
+    [1, 1, 0],
+    [0, 1, 1]
+  ],
+  [[1]]
 ];
-
-let backwardLBlock = [
-  [0, 0, 1],
-  [0, 0, 1],
-  [1, 1, 1]
-];
-
-let medLBlock = [
-  [1, 0],
-  [1, 0],
-  [1, 1]
-];
-let backwardMedLBock = [
-  [0, 1],
-  [0, 1],
-  [1, 1]
-];
-
-let smallLBlock = [
-  [1, 0],
-  [1, 1]
-];
-
-let backwardSamllLBlock = [
-  [0, 1],
-  [1, 1]
-];
-
-let threeSquare = [
-  [1, 1, 1],
-  [1, 1, 1],
-  [1, 1, 1]
-];
-
-let twoSquare = [
-  [1, 1],
-  [1, 1]
-];
-
-let tBlock = [
-  [1, 1, 1],
-  [0, 1, 0]
-];
-
-let threeLineBlock = [
-  [1, 1, 1]
-];
-
-let fourLineBlock = [
-  [1, 1, 1, 1]
-];
-
-let fiveLineBlock = [
-  [1, 1, 1, 1, 1]
-];
-
-let sBlock = [
-  [0, 1, 1],
-  [1, 1, 0]
-];
-
-let zBlock = [
-  [1, 1, 0],
-  [0, 1, 1]
-];
-
-let oneSquareBlock = [
-  [1]
-];
-
-let grid = [];
-let gridSize = 8;
-let cellSize = 50;
-let currentBlocks = [];
-let score = 0;
-let selectedBlock = null;
-let mouseOffset = { x: 0, y: 0 };
-let colors = ["yellow", "pink", "purple", "green", "blue", "orange"];
-let clickSound;
-let excellentSound;
 
 function setup() {
-  createCanvas(gridSize * cellSize + 400, gridSize * cellSize + 100);
-  initializeGrid();
-  generateBlocks();
-  clickSound.amp(1.0);
-  excellentSound.amp(1.0);
+  createCanvas(600, 600);
+  game = new Game(8, 50);
+  game.initializeGrid();
 }
 
 function draw() {
-  background(184, 245, 255);
-  drawGrid();
-  drawBlocks();
-  displayScore();
-  
-}
-function preload(){
-  clickSound = loadSound("click.wav");
-  excellentSound = loadSound("excellent.mp3");
-}
-
-
-function initializeGrid() {
-  for (let i = 0; i < gridSize; i++) {
-    grid[i] = [];
-    for (let j = 0; j < gridSize; j++) {
-      grid[i][j] = 0;
-    } 
-  }
-}
-
-function drawGrid() {
-  for (let i = 0; i < gridSize; i++) {
-    for (let j = 0; j < gridSize; j++) {
-      fill(grid[i][j] ? 'black' : 'white');
-      stroke(0);
-      rect(j * cellSize + 20, i * cellSize + cellSize, cellSize, cellSize);
-    }
-  }
-}
-
-function generateBlocks() {
-  currentBlocks = [];
-  for (let i = 0; i < 3; i++) {
-    let block = [];
-    let blockSize = int(random(1, 6)); // Random block size (1 to 5 cells)
-    for (let j = 0; j < blockSize; j++) {
-      block.push([j, 0]); // Simple horizontal blocks for simplicity
-    }
-    currentBlocks.push(block);
-  }
-}
-
-function drawBlocks() {
-  let xOffset = gridSize * cellSize + 20;
-  for (let i = 0; i < currentBlocks.length; i++) {
-    let block = currentBlocks[i];
-    for (let j = 0; j < block.length; j++) {
-      let x = xOffset + block[j][0] * cellSize + (i === selectedBlock ? mouseX - mouseOffset.x : 0);
-      let y = 100 + i * 100 + block[j][1] * cellSize + (i === selectedBlock ? mouseY - mouseOffset.y : 0);
-      fill("yellow");
-      stroke(0);
-      rect(x + 20, y + 20, cellSize, cellSize);
-    }
-  }
-}
-
-function displayScore() {
-  fill(0);
-  textSize(16);
-  text('Score: ' + score, gridSize * cellSize + 40, 50);
+  game.update();
+  game.draw();
 }
 
 function mousePressed() {
-  let xOffset = gridSize * cellSize + 20;
-  for (let i = 0; i < currentBlocks.length; i++) {
-    let block = currentBlocks[i];
-    for (let j = 0; j < block.length; j++) {
-      let x = xOffset + block[j][0] * cellSize;
-      let y = 100 + i * 100 + block[j][1] * cellSize;
-      if (mouseX > x && mouseX < x + cellSize && mouseY > y && mouseY < y + cellSize) {
-        selectedBlock = i;
-        mouseOffset.x = mouseX;
-        mouseOffset.y = mouseY;
+  if (game.state === "start") {
+    game.resetGame();
+  } else if (game.state === "end") {
+    game.resetGame();
+  } else if (game.state === "playing") {
+    for (let block of game.currentBlocks) {
+      if (block.containsPoint(mouseX, mouseY, game.cellSize)) {
+        game.draggedBlock = block;
+        game.offset.x = mouseX - block.x;
+        game.offset.y = mouseY - block.y;
+        block.isDragging = true;
         return;
       }
     }
+  }
+}
+
+function mouseDragged() {
+  if (game.draggedBlock) {
+    game.draggedBlock.x = mouseX - game.offset.x;
+    game.draggedBlock.y = mouseY - game.offset.y;
   }
 }
 
 function mouseReleased() {
-  if (selectedBlock !== null) {
-    placeBlock(selectedBlock);
-    selectedBlock = null;
+  if (game.draggedBlock) {
+    game.placeBlock(game.draggedBlock);
+    game.draggedBlock.isDragging = false;
+    game.draggedBlock = null;
   }
 }
-
-function placeBlock(blockIndex) {
-  let block = currentBlocks[blockIndex];
-  let gridX = floor(mouseX / cellSize);
-  let gridY = floor(mouseY / cellSize);
-
-  if (gridX >= 0 && gridX < gridSize && gridY >= 0 && gridY < gridSize) {
-    for (let i = 0; i < block.length; i++) {
-      let x = gridX + block[i][0];
-      let y = gridY + block[i][1];
-      if (x < gridSize && y < gridSize && grid[y][x] === 0) {
-        grid[y][x] = 1;
-      } 
-      else { 
-        return;
-      }
-    }
-    currentBlocks.splice(blockIndex, 1);
-    checkRowsAndColumns();
-    if (currentBlocks.length === 0) {
-      generateBlocks();
-    }
-  }
-}
-
-function checkRowsAndColumns() {
-  for (let i = 0; i < gridSize; i++) {
-    if (grid[i].every(cell => cell === 1)) {
-      grid[i] = grid[i].map(() => 0);
-      score += gridSize;
-    }
-    if (grid.every(row => row[i] === 1)) {
-      for (let j = 0; j < gridSize; j++) {
-        grid[j][i] = 0;
-      }
-      score += gridSize;
-    }
-  }
-}
-
-drawStartScreen() {
-  background(184, 245, 255);
-  fill(0);
-  textAlign(CENTER, CENTER);
-  textSize(48);
-  text("BlockBlast", width / 2, height / 2 - 50);
-  textSize(24);
-  text("Click to Start", width / 2, height / 2);
-}
-
-drawEndScreen() {
-  background(184, 245, 255);
-  fill(0);
-  textAlign(CENTER, CENTER);
-  textSize(48);
-  text("Game Over", width / 2, height / 2 - 50);
-  textSize(24);
-  text("Score: " + this.score, width / 2, height / 2);
-  text("High Score: " + this.highScore, width / 2, height / 2 + 30);
-  text("Click to Play Again", width / 2, height / 2 + 60);
-}
-
-resetGame(){
-  this.score = 0;
-  this.initializeGrid();
-  this.generateBlocks();
-  this.state = "playing";
-}
-
